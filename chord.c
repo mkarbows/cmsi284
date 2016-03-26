775b7749836fe1f20b00196a9ba71163ff569bf9
Megan Karbowski <Megan@MacBook-Pro.local>
2016-03-26 14:41:44 -0700

    added pointers to majorKey and minorKey functions

--- chord.c
+++ chord.c
@@ -11,20 +11,22 @@
 #include <stdlib.h> //malloc in here
 
 
-char* keySig[] = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
-int length = 12;
+char* keySig[12] = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
+//int length = 12;
 
 char* majorKey(char* key) {
     
 
     //char* major[(char) 6];
     //char* major[(char)malloc(sizeof(6))];
-    char major[6];
+    //char major[6];
+    char* major = (char*)malloc(sizeof(6));
+
     
-    for (int i = 0; i < length; i++) {
-        major[i] = keySig[i];
-        major[i+1] = keySig[i+4];
-        major [i+2] = keySig[i+4+3];
+    for (int i = 0; i < 12; i++) {
+        major[i] = *keySig[i];
+        major[i+1] = *keySig[i+4];
+        major [i+2] = *keySig[i+4+3];
     }
     return major;
     
@@ -34,11 +36,12 @@ char* majorKey(char* key) {
 char* minorKey(char* key) {
     char* minor = (char*)malloc(sizeof(6));
     
-    for (int i = 0; i < length; i++) {
-        minor[i] = keySig[i];
-        minor[i+1] = keySig[i+3];
-        minor [i+2] = keySig[i+3+3];
+    for (int i = 0; i < 12; i++) {
+        minor[i] = *keySig[i];
+        minor[i+1] = *keySig[i+3];
+        minor [i+2] = *keySig[i+3+3];
     }
+    return minor;
 }
 
 int main() {
---
+++ interval.c
@@ -0,0 +1,9 @@
+//
+//  interval.c
+//  
+//
+//  Created by Megan Karbowski on 3/26/16.
+//
+//
+
+#include "interval.h"
