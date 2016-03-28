aa149cf673ec38a7d9d3b50922370aa7e9b64048
Megan Karbowski <Megan@MacBook-Pro.local>
2016-03-27 19:08:13 -0700

    implementing finding the interval, compiles with no erros but doesn't print out interval name

--- interval.c
+++ interval.c
@@ -5,11 +5,56 @@
 //  Created by Megan Karbowski on 3/26/16.
 //
 //
-
+#include <string.h>
+#include <stdio.h>
 
 char* keys[] = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
 
+char* interval[] = {
+    "perfect octave",
+    "minor second",
+    "major second",
+    "minor third",
+    "major third",
+    "perfect fourth",
+    "tritone",
+    "perfect fifth",
+    "minor sixth",
+    "major sixth",
+    "minor seventh",
+    "major seventh",
+    "perfect octave"
+};
+
+void findInterval(char* key1, char* key2) {
+    int firstKey;
+    int secondKey;
+    int difference;
+    for (int i = 0; i < 13; i++) {
+        if (strcmp(key1, keys[i]) == 0) {
+            firstKey = i;
+            break;
+        }
+        if (strcmp(key2, keys[i]) ==0) {
+            secondKey = i;
+            break;
+        }
+    }
+    difference = ((secondKey - firstKey)%13);
+    
+    char* intName;
+    
+    for (int i = 0; i < 13; i++) {
+        if (difference == i) {
+            intName = interval[i];
+        }
+    }
+    
+    printf("%s to %s is a %s.\n", key1, key2, intName);
+}
+
 int main(int argc, char* argv[]) {
+    findInterval(argv[2], argv[3]);
     
     return 0;
 }
