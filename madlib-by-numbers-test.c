0b44bdbb5b333d30460205320b3745c8a6606369
Megan Karbowski <Megan@MacBook-Pro.local>
2016-03-29 00:50:06 -0700

    need to run more tests, fixed header files and made sure they were linked correctly

--- madlib-by-numbers-test.c
+++ madlib-by-numbers-test.c
@@ -6,4 +6,26 @@
 //
 //
 
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "madlib-by-numbers-test.h"
+
+//#include "madlib-by-numbers.c"
+#include "madlib-by-numbers.h"
+#include <assert.h>
+
+int main() {
+    
+    char* testStr = "The brilliant git likes to swim in the brilliant moonlight.";
+    
+    //char* template = "The 1 2 likes to 0 in the 1 moonlight.";
+    //int word_count = 3;
+    char* words[] = {"swim", "brilliant", "git"};
+    
+    char* testStr2 = madlib_by_numbers("The 1 2 likes to 0 in the 1 moonlight.", 3, words);
+    
+    //
+    assert(strcmp(testStr, testStr2)==0);
+    
+}
