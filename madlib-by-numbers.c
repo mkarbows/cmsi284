bdbda6105da8fee8178539d473679d22526d6213
Megan Karbowski <Megan@MacBook-Pro.local>
2016-03-29 00:49:19 -0700

    got my function to work, made a function to find the length needed to calloc... need to run more tests

--- madlib-by-numbers.c
+++ madlib-by-numbers.c
@@ -10,36 +10,57 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+//#include <assert.h>
 
 //madlib_by_numbers("The 1 2 likes to 0 in the 1 moonlight.", "swim", "brilliant", "git")
 //
 //"The brilliant git likes to swim in the brilliant moonlight."
 
+int newLen(char* template, int word_count, char* words[]) {
+    int count = 0;
+    int stringlen = strlen(template);
+    int digit;
+    for (int i = 0; i < stringlen; i++) {
+        if (isdigit(template[i])) {
+            digit = (template[i] - '0');
+            count = count + strlen(words[digit]);
+        } else {
+            count++;
+        }
+    }
+    return count;
+}
+
 char* madlib_by_numbers(char* template, int word_count, char* words[]) {
-    char* wordArray[sizeof((char*) words)];
     
-    int size = (strlen(template) + (sizeof((char*) words)));
-    int total = 0;
-    for (int i = 0; i < size; i++) {
-        total += strlen(template[i] + words[i]);
-    }
+    //char* concatStr = (char*)malloc(sizeof(char) * length + 1);
+    //int numArray[1000] = {-1};
+    //char* concatStr = calloc(1024, 1);
+    //char* concatStr = (char*)malloc(sizeof(char) * length + 1);
     
-    char* concatStr = (char*)malloc(sizeof(char) * (total + 1));
+    int length = newLen(template, word_count, words);
+    char* concatStr = calloc(length + 1, 1);
     
-    if (word_count != sizeof((char*) words)) {
-        printf("please enter the same amount of words as your word count");
-    }
-    
-    int numArray[10];
+    int digit;
+    int wordlen;
+    int pointer = 0;
     
     for (int i = 0; i < strlen(template); i++) {
         if (isdigit(template[i])) {
-            numArray[i] = atoi(&template[i]);
+            digit = (template[i] - '0');
+            wordlen = strlen(words[digit]);
+            strcat(&concatStr[pointer], words[digit]);
+            pointer = pointer + wordlen;
+        } else {
+            concatStr[pointer] = template[i];
+            pointer++;
         }
     }
+
+    printf("%s\n", concatStr);
+    return concatStr;
     
-    for (int i = 0; i < strlen(template); i++) {
-        strncat(concatStr, words[numArray[i]], strlen(words[numArray[i]]));
-    }
     
 }
