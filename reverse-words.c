8e690e82b4786916819405617e80d1f13ae5459e
Megan Karbowski <Megan@MacBook-Pro.local>
2016-03-29 15:26:59 -0700

    rearranged my original sequence of events and fixed my starting and ending indexs accordingly

--- reverse-words.c
+++ reverse-words.c
@@ -6,9 +6,56 @@
 //
 //
 
-#include "reverse-words.h"
-//#include "reverse-range-in-place.c"
+//#include "reverse-words.h"
+#include "reverse-range-in-place.c"
+
+#include <string.h>
 
 void reverse_words(char* string) {
     
+    
+    //reverse_range_in_place(string, startIndex, endIndex);
+    
+    int startIndex = 0;
+    int endIndex = 0;
+    
+    for (int i = 0; i < strlen(string); i++) {
+        
+        
+        
+        if (string[i] == ' ') {
+            reverse_range_in_place(string, startIndex, endIndex - 1);
+            startIndex = i + 1;
+            endIndex = i + 1;
+            
+        } else {
+            endIndex++;
+        }
+        
+    }
+    reverse_range_in_place(string, startIndex, endIndex);
+    reverse_range_in_place(string, 0, strlen(string));
 }
+
+//void reverse_words(char* string) {
+//    int startIndex = 0;
+//    int endIndex = strlen(string) - 1;
+//    
+//    reverse_range_in_place(string, startIndex, endIndex);
+//    for (int i = 0; i < strlen(string); i++) {
+//        
+//        //|| i == strlen(string) -1
+//        
+//        if (string[i] == ' ') {
+//            endIndex = i - 1;
+//            reverse_range_in_place(string, startIndex, endIndex);
+//            startIndex = i + 1;
+//            endIndex = i + 1;
+//            
+//        } else {
+//            endIndex++;
+//        }
+//        //endIndex = strlen(string);
+//        
+//    }
+//}
