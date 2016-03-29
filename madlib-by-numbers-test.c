3a6b2e310b4191cd837fac140fccb8e9b0a03851
Megan Karbowski <Megan@MacBook-Pro.local>
2016-03-29 01:42:44 -0700

    test if digits greater than word_count are passed over

--- madlib-by-numbers-test.c
+++ madlib-by-numbers-test.c
@@ -17,15 +17,29 @@
 
 int main() {
     
-    char* testStr = "The brilliant git likes to swim in the brilliant moonlight.";
+//    char* testStr = "The brilliant git likes to swim in the brilliant moonlight.";
+//    char* words[] = {"swim", "brilliant", "git"};
+//    
+//    char* testStr2 = madlib_by_numbers("The 1 2 likes to 0 in the 1 moonlight.", 3, words);
+//
+//    assert(strcmp(testStr, testStr2)==0);
     
-    //char* template = "The 1 2 likes to 0 in the 1 moonlight.";
-    //int word_count = 3;
-    char* words[] = {"swim", "brilliant", "git"};
     
-    char* testStr2 = madlib_by_numbers("The 1 2 likes to 0 in the 1 moonlight.", 3, words);
+//    char* testStr = "The fat dog likes to eat in the raging moonlight.";
+//    
+//    char* words[] = {"eat", "fat", "dog", "raging"};
+//    
+//    char* testStr2 = madlib_by_numbers("The 1 2 likes to 0 in the 3 moonlight.", 4, words);
+//    
+//    assert(strcmp(testStr, testStr2)==0);
     
-    //
+    
+    char* testStr = "The fat dog likes to eat in the 5 raging moonlight.";
+    
+    char* words[] = {"eat", "fat", "dog", "raging"};
+    
+    char* testStr2 = madlib_by_numbers("The 1 2 likes to 0 in the 5 3 moonlight.", 4, words);
+    
     assert(strcmp(testStr, testStr2)==0);
     
 }
