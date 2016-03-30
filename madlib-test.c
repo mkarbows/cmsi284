ac47ec69d42953fc9f36db41c4b8409ee98ce6ed
Megan Karbowski <Megan@MacBook-Pro.local>
2016-03-29 20:19:53 -0700

    added another test

--- madlib-test.c
+++ madlib-test.c
@@ -18,9 +18,16 @@ int main() {
     char* adjective = "brilliant";
     char* noun = "git";
     char* verb = "swim";
-    
     char* madlibStr = madlib(template, adjective, noun, verb);
     char* testStr = "The brilliant git likes to swim in the moonlight.";
-    
     assert(strcmp(madlibStr, testStr)==0);
+    
+    //test supposed to fail here and it does, yay
+    char* template2 = "The \%s \%s likes to \%s in the moonlight.";
+    char* adjective2 = "hi";
+    char* noun2 = "hey";
+    char* verb2 = "hello";
+    char* madlibStr2 = madlib(template2, adjective2, noun2, verb2);
+    char* testStr2 = "The brilliant git likes to swim in the moonlight.";
+    assert(strcmp(madlibStr2, testStr2)==0);
 }
