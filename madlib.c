18cc1b09f494b310587c944351fbfff13ba95b38
Megan Karbowski <mkarbows@lion.lmu.edu>
2016-03-29 23:52:25 -0700

    final commit madlib.c
    terminal commits did not work so there are more before this that didn't get pushed correctly...

--- madlib.c
+++ madlib.c
@@ -6,13 +6,25 @@
 //
 //
 
-#include "madlib.h"
+//#include "madlib.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 char* madlib(char* template, char* adjective, char* noun, char* verb) {
-    
-    static char strMax[100];
+    int count = 0;
+    for (int i = 0; i < strlen(template); i++) {
+        if(template[i] == '%' && template[i + 1] == 's') {
+            count++;
+        }
+    }
+    if (count != 3) {
+        return NULL;
+    }
+
+    char* strMax = (char*)malloc(sizeof(char) * strlen(template) + strlen(adjective) + strlen(noun)+strlen(verb));
     int result = sprintf(strMax, template, adjective, noun, verb);
-    
     return strMax;
+    free(strMax);
 }
