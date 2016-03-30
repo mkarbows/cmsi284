21a0dd0e5493b7b74836beffb505115690eee2e4
Megan Karbowski <Megan@MacBook-Pro.local>
2016-03-29 23:47:17 -0700

    added for loop/if statement to make sure the user only inputs three %s in their template

--- madlib.c
+++ madlib.c
@@ -13,6 +13,16 @@
 #include <stdlib.h>
 
 char* madlib(char* template, char* adjective, char* noun, char* verb) {
+    int count = 0;
+    for (int i = 0; i < strlen(template); i++) {
+        if(template[i] == '%' && template[i + 1] == 's') {
+            count++;
+        }
+    }
+    if (count != 3) {
+        return NULL;
+    }
+
     char* strMax = (char*)malloc(sizeof(char) * strlen(template) + strlen(adjective) + strlen(noun)+strlen(verb));
     int result = sprintf(strMax, template, adjective, noun, verb);
     return strMax;
