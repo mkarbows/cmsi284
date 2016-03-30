66dece81cad642dc93955a3731e3c6d256fd881a
Megan Karbowski <Megan@MacBook-Pro.local>
2016-03-29 19:29:44 -0700

    linked header files, need to malloc space

--- madlib.c
+++ madlib.c
@@ -6,13 +6,13 @@
 //
 //
 
-#include "madlib.h"
+//#include "madlib.h"
 
+#include <stdio.h>
+#include <string.h>
 
 char* madlib(char* template, char* adjective, char* noun, char* verb) {
-    
     static char strMax[100];
     int result = sprintf(strMax, template, adjective, noun, verb);
-    
     return strMax;
 }
