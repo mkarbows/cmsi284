e1cc73eb82b147c7850f3236956a2a37784ad28e
Megan Karbowski <Megan@MacBook-Pro.local>
2016-03-29 19:31:00 -0700

    linked header files

--- madlib-test.c
+++ madlib-test.c
@@ -6,33 +6,12 @@
 //
 //
 
-#include "madlib-test.h"
+#include "madlib.h"
 
-#include "madlib.c"
 #include <assert.h>
 #include <string.h>
-
-//madlib("The \%s \%s likes to \%s in the moonlight.", "brilliant", "git", "swim")
-
-//void reverse_range_in_place(char* string, int start_index, int end_index);
-//char* madlib(char* template, char* adjective, char* noun, char* verb) ??
-
-//#define MAX_TEST_STRING_LENGTH 20
-
-//void madlib-test() {
-////    char* original_string = "Reverse my insides!";
-////    char test_string[MAX_TEST_STRING_LENGTH];
-////    
-////    //strncpy(test_string, original_string, MAX_TEST_STRING_LENGTH);
-////    
-////    char* madlib(char* template, char* adjective, char* noun, char* verb);
-////    reverse_range_in_place(test_string, 9, 5);
-////    assert(strncmp(original_string, test_string, MAX_TEST_STRING_LENGTH) == 0);
-//    
-//    
-//    
-//    
-//}
+#include <stdio.h>
+#include <string.h>
 
 int main() {
     char* template = "The \%s \%s likes to \%s in the moonlight.";
@@ -40,9 +19,8 @@ int main() {
     char* noun = "git";
     char* verb = "swim";
     
-    char* madlibs = madlib(template, adjective, noun, verb);
+    char* madlibStr = madlib(template, adjective, noun, verb);
     char* testStr = "The brilliant git likes to swim in the moonlight.";
     
-    assert(strcmp(madlibs, testStr)==0);
-
+    assert(strcmp(madlibStr, testStr)==0);
 }
