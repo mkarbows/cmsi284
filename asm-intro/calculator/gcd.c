15b03054c8339ee1b09058238ee48b3ca3788189
Megan Karbowski <mkarbows@lion.lmu.edu>
2016-05-02 22:33:21 -0700

    gcd.c commit

---
+++ asm-intro/calculator/gcd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+int64_t gcd(int64_t, int64_t);
+
+int main(int argc, char** argv) {
+	if (argc != 3) {
+		printf("Please enter 3 arguments\n");
+		return 0;
+	}
+	int firstArg = atoi(argv[1]);
+	int secondArg = atoi(argv[2]);
+	printf("%ld\n", gcd(firstArg, secondArg));
+}
