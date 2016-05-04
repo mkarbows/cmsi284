0524a1244f7b4e0ad77f477034e07c5e21d7afc8
Megan Karbowski <mkarbows@lion.lmu.edu>
2016-05-03 22:45:43 -0700

    update build.sh

--- asm-intro/calculator/build.sh
+++ asm-intro/calculator/build.sh
@@ -4,10 +4,10 @@ clear
 
 nasm -felf64 calc-ops.asm
 
-gcc calc-ops.o addition.c -o addition
+gcc calc-ops.o add.c -o add
 
 gcc calc-ops.o subtract.c -o subtract
 
 gcc calc-ops.o gcd.c -o gcd
 
-echo To call each function say [addition, subtract, gcd] {arg1} {arg2}
+echo To call each function say [add, subtract, gcd] {arg1} {arg2}
